685679c369d0bbb1a6442fea70b7329d59726011
zacharygilliom <zacharygilliom@gmail.com>
2020-05-09 17:10:01 -0400

    Added new test files, and update getSplitMessage method to handle messages titled with hyphens and no spaces.

--- src/link.py
+++ src/link.py
@@ -66,53 +66,72 @@ class emailMessage:
         split_base = os.path.splitext(message_base)
         # print(f'Split_base = {split_base}')
         message_no_ext = split_base[0].split()
-        return message_no_ext
-
-    def splitHyphenatedMessage(self):
-        email_message = self.getSplitMessage()
-        hyphen_message = email_message[0]
-        split_hyphen_message = hyphen_message.split('-')
-        return split_hyphen_message
-
-    def matchInternalOrder(self, order_list):
-        # iterate with index through order list
-        for i, word in enumerate(order_list):
-            # casefold is a string method to ignore case
-            word = str(word).casefold()
-            # if one of the words in the list is equal to ajh, below are all the different variants of how it could be.
-            if word[:3] == 'ajh':
-                if len(word) == 9 and word[3] == '0':
-                    return word[:3] + '0' + word[4:]
-                elif len(word) == 9 and word[3] != '0':
-                    return word[:3] + '0' + word[4:]
-                elif len(word) == 8: 
-                    return word[:3] + '0' + word[3:]
-                elif len(word) == 3:
-                    if len(order_list[i+1]) == 6:
-                        new_word = order_list[i] + order_list[i+1]
-                        return new_word
-                    elif len(order_list[i+1]) == 5:
-                        new_word = order_list[i] + '0' + order_list[i+1]
-                        return new_word
-                    else:
-                        return False
-                else:
-                    return False
+        final_message = []
+        for mess in message_no_ext:
+            if len(mess) > 14:
+               word = mess.split('-') 
+               for w in word:
+                   final_message.append(w)
             else:
-                return False
+                final_message.append(mess)
+        return final_message
 
-    def matchExternalOrder(self, order_list):
+
+    def matchInternalOrder(self):
+        order_list = self.getSplitMessage()
+        # iterate with index through order list
+        for i, word in enumerate(order_list): 
+            word = str(word).casefold()
+            if len(word) > 14:
+                split_word = word.split('-')
+                for j, words in enumerate(split_word):
+                    words = str(words).casefold()
+                    if words[:3] == 'ajh':
+                        if len(words) == 9 and words[3] == '0':
+                            word = words[:3] + '0' + words[4:]
+                            return word
+                        elif len(words) == 9 and words[3] != '0':
+                            word = words[:3] + '0' + words[4:]
+                            return word
+                        elif len(words) == 8: 
+                            word = words[:3] + '0' + words[3:]
+                            return word
+                        elif len(words) == 3:
+                            if len(words[j+1]) == 6:
+                                word = split_word[j] + split_word[j+1]
+                                return word
+                            elif len(words[j+1]) == 5:
+                                word = split_word[j] + '0' + split_word[j+1]
+                                return word
+            else:
+            # casefold is a string method to ignore case
+            # if one of the words in the list is equal to ajh, below are all the different variants of how it could be.
+                if word[:3] == 'ajh':
+                    if len(word) == 9 and word[3] == '0':
+                        return word[:3] + '0' + word[4:]
+                    elif len(word) == 9 and word[3] != '0':
+                        return word[:3] + '0' + word[4:]
+                    elif len(word) == 8: 
+                        return word[:3] + '0' + word[3:]
+                    elif len(word) == 3:
+                        if len(order_list[i+1]) == 6:
+                            new_word = order_list[i] + order_list[i+1]
+                            return new_word
+                        elif len(order_list[i+1]) == 5:
+                            new_word = order_list[i] + '0' + order_list[i+1]
+                            return new_word
+
+    def matchExternalOrder(self):
         ext_order_numbers = self.external_order_numbers
         # External order numbers can have two different lengths, 4 or 5, but they are generally fully type out and on
         # differently typed like they are in internal order numbers.
-        for word in order_list:
+        for word in self.getSplitMessage():
+            word = str(word).casefold()
             for number in ext_order_numbers:
-                if word[:5] == number:
+                if word[:5] == number.casefold():
                     return word
-                elif word[:4] == number:
+                elif word[:4] == number.casefold():
                     return word
-                else:
-                    return False
 
 def linkFiles(workbook, direc):
     # Activate the sheet in our workbook.
@@ -123,15 +142,10 @@ def linkFiles(workbook, direc):
             # [0] in dir_value is our order number, [1] is our file location of the file.
             link_number = emailMessage(dir_value[0], ext_order_numbers)
             link_location = dir_value[1]
-            print(link_number.splitHyphenatedMessage())
-            if link_number.matchInternalOrder(link_number.getSplitMessage()):
-                linked_val = link_number.matchInternalOrder(link_number.getSplitMessage())
-            elif link_number.matchExternalOrder(link_number.getSplitMessage()):
-                linked_val = link_number.matchExternalOrder(link_number.getSplitMessage())
-            elif link_number.matchInternalOrder(link_number.splitHyphenatedMessage()):
-                linked_val = link_number.matchInternalOrder(order_list=link_number.splitHyphenatedMessage())   
-            elif link_number.matchExternalOrder(link_number.splitHyphenatedMessage()):
-                linked_val = link_number.matchExternalOrder(order_list=link_number.splitHyphenatedMessage())
+            if link_number.matchInternalOrder():
+                linked_val = link_number.matchInternalOrder()
+            elif link_number.matchExternalOrder():
+                linked_val = link_number.matchExternalOrder()
             else:
                 linked_val = ""
             currentRow = 2
@@ -147,15 +161,15 @@ def linkFiles(workbook, direc):
                     currentRow += 1
                     
         # WSL 2 location
-        # workbook.save(filename='/home/zach/python-projects/Excel-Hyperlinks/workbooks/Change Log Updated.xlsx')
+        workbook.save(filename='/home/zach/python-projects/Excel-Hyperlinks/workbooks/Change Log Updated.xlsx')
         # Fedora Linux location
-        workbook.save(filename='/home/zacharygilliom/Documents/python-projects/Excel-Hyperlinks/workbooks/Change Log Updated.xlsx')
+        # workbook.save(filename='/home/zacharygilliom/Documents/python-projects/Excel-Hyperlinks/workbooks/Change Log Updated.xlsx')
 
 # Specify the path to the source of our email files to link to our workbook.
 # Fedora Linux path
-Folder_path = Path("/home/zacharygilliom/Documents/python-projects/Excel-Hyperlinks/files/")
+# Folder_path = Path("/home/zacharygilliom/Documents/python-projects/Excel-Hyperlinks/files/")
 # WSL 2 path
-#Folder_path = Path("/home/zach/python-projects/Excel-Hyperlinks/files/")
+Folder_path = Path("/home/zach/python-projects/Excel-Hyperlinks/files/")
 
 # Speciy the external order numbers that we will match.
 ext_order_numbers = external_order_numbers
@@ -168,9 +182,9 @@ userDirectoryFiles = userDirectory.zipFilesAndPath()
 
 # Open up our workbook specifying the path to it via openpxl method.
 # Fedora Linux path
-book = load_workbook(filename='/home/zacharygilliom/Documents/python-projects/Excel-Hyperlinks/workbooks/Change Log.xlsx')
+# book = load_workbook(filename='/home/zacharygilliom/Documents/python-projects/Excel-Hyperlinks/workbooks/Change Log.xlsx')
 # WSL 2 path
-# book = load_workbook(filename='/home/zach/python-projects/Excel-Hyperlinks/workbooks/Change Log.xlsx')
+book = load_workbook(filename='/home/zach/python-projects/Excel-Hyperlinks/workbooks/Change Log.xlsx')
 
 # call function with our open workbook and our directory class.
 linkFiles(workbook=book, direc = userDirectoryFiles)
